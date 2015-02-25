2a1c9926fe5feceba339db872a9d85642bafb773
AchimTuran <Achim.Turan@o2online.de>
2015-02-25 14:02:16 +0100

    [Addon] Added BiQuad processing method

--- src/DSPProcessor.cpp
+++ src/DSPProcessor.cpp
@@ -86,126 +86,20 @@ CDSPProcessor::~CDSPProcessor()
   } 
 }
 
-bool CDSPProcessor::InputProcess(const float **Array_in, unsigned int Samples)
-{
-	return true;
-}
-
-unsigned int CDSPProcessor::InputResampleProcess(float **Array_in, float **Array_out, unsigned int Samples)
-{
-	unsigned int currentChannelFlag = AE_DSP_PRSNT_CH_FL;
-	for( unsigned int ii = 0; ii < AE_DSP_CH_MAX; ii++ )
-	{
-		if(m_StreamSettings.lInChannelPresentFlags & currentChannelFlag)
-		{
-			memcpy( Array_out[ii], Array_in[ii], Samples*sizeof(float) );
-		}
-
-		currentChannelFlag <<= 1;
-	}
-
-	return Samples;
-}
-
-unsigned int CDSPProcessor::PreProcess(unsigned int Mode_id, float **Array_in, float **Array_out, unsigned int Samples)
-{
-	unsigned int currentChannelFlag = AE_DSP_PRSNT_CH_FL;
-	for( unsigned int ii = 0; ii < AE_DSP_CH_MAX; ii++ )
-	{
-		if(m_StreamSettings.lInChannelPresentFlags & currentChannelFlag)
-		{
-			memcpy( Array_out[ii], Array_in[ii], Samples*sizeof(float) );
-		}
-
-		currentChannelFlag <<= 1;
-	}
-
-	return Samples;
-}
-	
-unsigned int CDSPProcessor::MasterProcess(float **Array_in, float **Array_out, unsigned int Samples)
-{
-	ADSP_CHANNEL_LAYOUT sourceChannelLayout = getChannelLayout(m_StreamSettings.lInChannelPresentFlags);
-	ADSP_CHANNEL_LAYOUT sinkChannelLayout	= getChannelLayout(m_StreamSettings.lOutChannelPresentFlags);
-
-	switch(sinkChannelLayout)
-	{
-		case channellayout_stereo:
-			from_X_to_stereo( Array_in, Array_out, Samples, sourceChannelLayout );
-		break;
-
-		case channellayout_2_1:
-			from_X_to_2_1( Array_in, Array_out, Samples, sourceChannelLayout );
-		break;
-
-		case channellayout_3_0:
-			from_X_to_3_0( Array_in, Array_out, Samples, sourceChannelLayout );
-		break;
-		
-		case channellayout_quad_side:
-			from_X_to_quad_side( Array_in, Array_out, Samples, sourceChannelLayout );
-		break;
-
-		case channellayout_3_1:			
-			from_X_to_3_1( Array_in, Array_out, Samples, sourceChannelLayout );
-		break;
-
-		case channellayout_5_0:
-			from_X_to_5_0_side( Array_in, Array_out, Samples, sourceChannelLayout );
-		break;
-
-		case channellayout_4_1_side:
-			from_X_to_4_1_side( Array_in, Array_out, Samples, sourceChannelLayout );
-		break;
-
-		case channellayout_5_1_side:
-			from_X_to_5_1_side( Array_in, Array_out, Samples, sourceChannelLayout );
-		break;
-
-		case channellayout_7_0:
-			from_X_to_7_0( Array_in, Array_out, Samples, sourceChannelLayout );
-		break;
-
-		case channellayout_7_1:
-			from_X_to_7_1( Array_in, Array_out, Samples, sourceChannelLayout );
-		break;
-	}
-
-	return Samples;
-}
-	
 unsigned int CDSPProcessor::PostProcess(unsigned int Mode_id, float **Array_in, float **Array_out, unsigned int Samples)
 {
-	unsigned int currentChannelFlag = AE_DSP_PRSNT_CH_FL;
-	for( unsigned int ii = 0; ii < AE_DSP_CH_MAX; ii++ )
-	{
-		if(m_StreamSettings.lOutChannelPresentFlags & currentChannelFlag)
-		{
-			memcpy( Array_out[ii], Array_in[ii], Samples*sizeof(float) );
-		}
+  for(int ii = 0; ii < m_MaxProcessingChannels; ii++)
+  {
+    ASPLIB_ERR err = CBiQuadFactory::calc_BiQuadSamples(m_ChannelHandle[ii].BiQuadHandle,
+                                                        Array_in[m_ChannelHandle[ii].ChannelID],
+                                                        Array_out[m_ChannelHandle[ii].ChannelID],
+                                                        Samples);
+    if(err != ASPLIB_ERR_NO_ERROR)
+    {
+      // ToDo: throw some error message!
+      return 0;
+    }
+  }
 
-		currentChannelFlag <<= 1;
-	}
-	return Samples;
-}
-
-unsigned int CDSPProcessor::OutputResampleProcess(float **Array_in, float **Array_out, unsigned int Samples)
-{
-	unsigned int currentChannelFlag = AE_DSP_PRSNT_CH_FL;
-	for( unsigned int ii = 0; ii < AE_DSP_CH_MAX; ii++ )
-	{
-		if(m_StreamSettings.lOutChannelPresentFlags & currentChannelFlag)
-		{
-			memcpy( Array_out[ii], Array_in[ii], Samples*sizeof(float) );
-		}
-
-		currentChannelFlag <<= 1;
-	}
-	return Samples;
-}
-
-int CDSPProcessor::MasterProcessGetOutChannels(unsigned long &Out_channel_present_flags)
-{
-	Out_channel_present_flags = m_StreamSettings.lInChannelPresentFlags;
-	return m_StreamSettings.iInChannels;
+  return Samples;
 }
